3ca55d1683fb6e9287b6e52defffa7fb0caf5196
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-06-14 09:06:51 +0700

    initial redux

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from "expo-status-bar";
 import Navigation from "./src/Navigation";
+import { Provider } from "react-redux";
 
 export default function App() {
 	return (
-		<>
+		<Provider>
 			<StatusBar style="auto" />
 			<Navigation />
-		</>
+		</Provider>
 	);
 }
--- src/Navigation.js
+++ src/Navigation.js
@@ -1,4 +1,4 @@
-import { Text, Pressable, View } from "react-native";
+import { Text, Pressable } from "react-native";
 import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer } from "@react-navigation/native";
