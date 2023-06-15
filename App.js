ae5998438c7055b3d07c7a375224199959ada97f
mrizkysolehudin <mrizkysolehudin11@gmail.com>
2023-06-15 09:53:23 +0700

    add setup env

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import Navigation from "./src/Navigation";
 import { Provider } from "react-redux";
 import { store } from "./src/redux";
+import { StripeProvider } from "@stripe/stripe-react-native";
+import config from "./config";
 
 export default function App() {
 	return (
 		<Provider store={store}>
 			<StatusBar style="auto" />
-			<Navigation />
+
+			<StripeProvider publishableKey={config.STRIPE_API_KEYS_TOKEN}>
+				<Navigation />
+			</StripeProvider>
 		</Provider>
 	);
 }
---
+++ config.js
@@ -0,0 +1,3 @@
+import { STRIPE_API_KEYS_TOKEN } from "@env";
+
+export default { STRIPE_API_KEYS_TOKEN };
